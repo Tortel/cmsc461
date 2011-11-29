a17bad263b93f51588fdbbd8af3cba5341058839
Scott <Tortel1210@gmail.com>
2011-11-29 13:46:20 -0500

    Renamed connect, and testing

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once('include/include.php');
 
 head("Welcome");
 
+$db = db_connect();
+
 startPost("Welcome to Mars Realty", "November 25, 2011");
 ?>
 <p> Welcome to Mars Realty! This will be more complete as I actually do it!</p>
