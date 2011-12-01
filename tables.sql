d3cd76cfb9c86836960f5907e632e11ac6d7df88
Scott <Tortel1210@gmail.com>
2011-11-30 23:00:11 -0500

    Trying to fix sql

--- tables.sql
+++ tables.sql
@@ -19,73 +19,73 @@ drop table Newspaper purge;
 
 create table Branch(
    id int primary key,
-   street varchar(250),
-   city varchar(250),
-   state varchar(2),
-   zip varchar(5),
-   phone varchar(15),
-   fax varchar(15),
-   manager refrences Manager(id)
+   street varchar2(250),
+   city varchar2(250),
+   state varchar2(2),
+   zip varchar2(5),
+   phone varchar2(15),
+   fax varchar2(15),
+   manager int refrences Manager(id)
 );
 
 create table Manager(
-   id refrences employee,
+   id int refrences employee,
    bonus number(*,2)
 );
 
 create table Employee(
    id int primary key,
-   firstName varchar(100),
-   lastName varchar(100),
+   firstName varchar2(100),
+   lastName varchar2(100),
    sex char,
    birthday Date,
    salary number(*,2),
-   branch refrences Branch(id)
+   branch int refrences Branch(id)
 );
 
 create table Supervisor(
-   id refrences Employee
+   id int refrences Employee
 );
 
 create table Associate(
-   id refrences Employee,
-   supervisor refrences Supervisor(id),
+   id int refrences Employee,
+   supervisor int refrences Supervisor(id),
    constraint check ( count(select s.id from Supervisor as s where s.id = id) <= 6 )
 );
 
 create table Client(
    id int primary key,
-   firstName varchar(100),
-   lastName varchar(100),
-   street varchar(250),
-   city varchar(250),
-   state varchar(2),
-   zip varchar(5),
-   phone varchar(15),
-   workPhone varchar(15),
+   firstName varchar2(100),
+   lastName varchar2(100),
+   street varchar2(250),
+   city varchar2(250),
+   state varchar2(2),
+   zip varchar2(5),
+   phone varchar2(15),
+   workPhone varchar2(15),
    propertyType int,
    maxRent number(*,2)
-   associate refrences Associate(id),
+   associate int refrences Associate(id),
    registerDate Date,
-   branchId refrences Branch(id)
+   branchId int refrences Branch(id)
 );
 
 create table Viewing(
    id int primary key,
    client refrences client(id),
-   associate refrences Associate(id),
-   propertyId refrences Property(id),
+   associate int refrences Associate(id),
+   propertyId int refrences Property(id),
    viewDate date,
-   comments varchar(5000)
+   comments varchar2(5000)
 );
 
 /* Might need to adjust rent, depending on adaptive rent service */
 create table Property(
    id int primary key,
-   street varchar(250),
-   city varchar(250),
-   state varchar(2),
-   zip varchar(5),
+   street varchar2(250),
+   city varchar2(250),
+   state varchar2(2),
+   zip varchar2(5),
    type int,
    bedrooms int (3,1),
    bathrooms number(3,1),
@@ -96,8 +96,8 @@ create table Property(
    lastUpdate date,
    maxRent number(*,2),
    minRent number(*,2),
-   associate refrences Associate(id),
-   owner refrences Owner(id),
+   associate int refrences Associate(id),
+   owner int refrences Owner(id),
    constraint check_boolean check (rented in ('Y', 'N')),
    constraint check_associate check ( count(select a.id from Associate as a where a.id = associate) <= 30 )
 );
@@ -109,47 +109,47 @@ create table Lease(
    deposit number(*,2),
    startDate date,
    endDate date,
-   client refrences client(id),
-   property refrences Propery(id),
-   associate refrences Associate(id)
+   client int refrences client(id),
+   property int refrences Propery(id),
+   associate int refrences Associate(id)
 );
 
 create table Owner(
    id int primary key,
-   name varchar(100),
-   street varchar(250),
-   city varchar(250),
-   state varchar(2),
-   zip varchar(5),
-   phone varchar(15),
-   fax varchar(15),
+   name varchar2(100),
+   street varchar2(250),
+   city varchar2(250),
+   state varchar2(2),
+   zip varchar2(5),
+   phone varchar2(15),
+   fax varchar2(15),
    isBusiness char(1).
    constraint check_boolean check (isBusiness in ('Y', 'N'))
 );
 
 create table Business(
-   id refrences Owner,
-   type varchar(100),
-   contactName varchar(100)
+   id int refrences Owner,
+   type varchar2(100),
+   contactName varchar2(100)
 );
 
 create table Advertisement(
-   property refrences Property(id),
+   property int refrences Property(id),
    printDate date,
    cost number(*,2),
-   newspaperId refrences Newspaper(id)
+   newspaperId int refrences Newspaper(id)
 );
 
 create table Newspaper(
    id int primary key,
-   name varchar(100),
-   street varchar(250),
-   city varchar(250),
-   state varchar(2),
-   zip varchar(5),
-   phone varchar(15),
-   fax varchar(15),
-   contactName varchar(100)
+   name varchar2(100),
+   street varchar2(250),
+   city varchar2(250),
+   state varchar2(2),
+   zip varchar2(5),
+   phone varchar2(15),
+   fax varchar2(15),
+   contactName varchar2(100)
 );
 
 /* Need a function to calculate the average popularity since a given date */
