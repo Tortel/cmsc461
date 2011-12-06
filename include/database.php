aa80e6c2482afc5b9767331a99b4de54b17d3a5b
Scott <Tortel1210@gmail.com>
2011-12-05 22:37:58 -0500

    Fix array error

--- include/database.php
+++ include/database.php
@@ -84,7 +84,6 @@ function dbDate($date){
  * Fetches all the rows from a query, returns a 2-dimensional array.
  */
 function dbFetchAll($query){
-   $array = [];
    oci_fetch_all($query, $array, 0, -1, OCI_FETCHSTATEMENT_BY_ROW+OCI_NUM);
    return $array;
 }
