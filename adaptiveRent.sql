764271a61fd7ac7ba5de77c10c9decb285754a2f
Scott <Tortel1210@gmail.com>
2011-12-15 17:22:32 -0500

    Broken, need to fix

--- adaptiveRent.sql
+++ adaptiveRent.sql
@@ -10,3 +10,22 @@ create or replace trigger adaptiveRentIncrease
    END;
 /
 
+create or replace trigger adaptiverentDecrease
+   after insert or update on Viewing
+   BEGIN
+      IF('select count(viewing.id) from viewing, property where propertyId = :NEW.propertyId and viewDate > lastUpdate' <
+   'select avg(count(viewing.id)) from viewing, property where viewing.propertyid = property.id and type = :NEW.type and zip = :NEW.zip and viewdate > lastUpdate group by viewing.propertyid') THEN
+          UPDATE property SET rent = 0.95*rent WHERE id = :NEW.propertyId;
+      update property set rent = minRent if rent < minRent;
+   END;
+/
+
+/*
+      update (Select rent from Property as P where (select count(viewing.id) from viewing where (propertyId = P.id and viewDate > P.lastupdate) < select avg(count(viewing.id)) from viewing, property where propertyid = property.id and viewdate > lastUpdate group by viewing.propertyid ) ) set rent = 0.95*rent; 
+      
+Select id from Property as P where (select count(viewing.id) from viewing, property where propertyId = P.propertyId and viewDate > P.lastUpdate) < (select avg(count(viewing.id)) from viewing, property where viewing.propertyid = property.id and viewdate > lastUpdate)
+      
+      IF('select count(viewing.id) from viewing, property where propertyId = :NEW.propertyId and viewDate > lastUpdate' <
+   'select avg(count(viewing.id)) from viewing, property where viewing.propertyid = property.id and type = :NEW.type and zip = :NEW.zip and viewdate > lastUpdate group by viewing.propertyid') THEN
+          UPDATE property SET rent = 0.95*rent WHERE id = :NEW.propertyId;
+      */
