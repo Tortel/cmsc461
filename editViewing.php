0f194e3e8fef0bff4dfe89dd677592e08f34a803
Scott <Tortel1210@gmail.com>
2011-12-14 22:41:21 -0500

    Edit Viewing

--- editViewing.php
+++ editViewing.php
@@ -2,41 +2,37 @@
 
 require_once('include/include.php');
 
-$db = dbConnect();
+$db = dbConnectt();
 
 if($_POST['submit']){
-   $rent = $_POST['rent'];
-   $deposit = $_POST['deposit'];
-   $start = dbDate($_POST['start']);
-   $end = dbDate($_POST['end']);
    $property = $_POST['property'];
    $client = $_POST['client'];
    $associate = $_POST['associate'];
+   $date = dbDate($_POST['date']);
+   $comments = $_POST['comments'];
    $id = $_POST['id'];
+   $comments = htmlspecialchars( str_replace(array('\r\n', '\n', '\r'), '<br />', $comments), ENT_QUOTES);
    
-   if( !(!$rent || !$deposit || !$start || !$end || !$property || !$associate || !$client) ){
-      //Create it
-      dbExec($db, "update Lease set rent = $rent, deposit = $deposit, startDate = $start, endDate = $end, client = $client, property = $property, associate = $associate where id = $id");
-      
-      header('Location: viewLease.php');
-   }
-
+   dbExec($db, "update viewing set client = $client, associate = $associate, propertyId = $property, viewDate = $date, comments = '$comments' where id = $id");
+   
+   header("Location: viewViewing.php");
 }
 
-head('View Lease');
+
+head('Edit Viewing');
 
 $id = $_GET['id'];
 
 if((!$id && $id != 0) || !is_numeric($id)){
-   $leaseQuery = dbExec($db, 'select id, TO_CHAR(startDate, \'DD.MM.YYYY\'), TO_CHAR(endDate, \'DD.MM.YYYY\'), property from lease');
+   $viewQuery = dbExec($db, 'select id, propertyId, TO_CHAR(viewDate, \'DD.MM.YYYY\') from viewing');
    
-   startPost('Select Lease');
+   startPost('Edit Viewing');
    ?>
    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="get">
       <select name="id" id="id">
       <?php
-         while( ($row = dbFetchRow($leaseQuery)) ){
-            echo '<option value="'.$row[0].'">'.$row[0].' - '.$row[1].' through '.$row[2].' for Property '.$row[3].'</option>';
+         while( ($row = dbFetchRow($viewQuery)) ){
+            echo '<option value="'.$row[0].'">'.$row[0].' - For Property '.$row[1].' on '.$row[2].'</option>';
          }
       ?>
       </select>
@@ -53,9 +49,9 @@ if((!$id && $id != 0) || !is_numeric($id)){
    exit();
 }
 
-startPost('Edit Lease');
+startPost('Edit Viewing');
 
-$query = dbExec($db, "select rent, deposit, TO_CHAR(startDate, 'MM.DD.YYYY'), TO_CHAR(endDate, 'MM.DD.YYYY'), property, client, associate from Lease");
+$query = dbExec($db, "select propertyId, client, associate, TO_CHAR(viewDate, 'MM.DD.YYYY'), comments from Viewing where id = $id");
 
 $row = dbFetchRow($query);
 
@@ -63,23 +59,8 @@ $row = dbFetchRow($query);
 
 <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
    <input type="hidden" id="submit" name="submit" value="1" />
+   <input type="hidden" id="id" name="id" value="<?php echo $id ?>" />
    <table>
-      <tr>
-         <td>Monthly Rent:</td>
-         <td><input type="text" size="30" id="rent" name="rent" value="<?php echo $row[0] ?>" /></td>
-      </tr>
-      <tr>
-         <td>Deposit:</td>
-         <td><input type="text" size="30" id="deposit" name="deposit" value="<?php echo $row[1] ?>" /></td>
-      </tr>
-      <tr>
-         <td>Start Date (Ex 12.10.2011):</td>
-         <td><input type="text" size="30" id="start" name="start" value="<?php echo $row[2] ?>" /></td>
-      </tr>
-      <tr>
-         <td>End Date (Ex 12.10.2012):</td>
-         <td><input type="text" size="30" id="end" name="end" value="<?php echo $row[3] ?>" /></td>
-      </tr>
       <tr>
          <td>Property:</td>
          <td>
@@ -87,9 +68,9 @@ $row = dbFetchRow($query);
             <?php
                $propertyQuery = dbExec($db, 'select id, street, city, state from property where rented = \'N\'');
                while( ($prow = dbFetchRow($propertyQuery)) ){
-                  if($prow[0] == $row[4]){
+                  if($row[0] == $prow[0]){
                      echo '<option value="'.$prow[0].'" selected>'.$prow[0].' - '.$prow[1].', '.$prow[2].', '.$prow[3].'</option>';
-                  } else {
+                  } echo {
                      echo '<option value="'.$prow[0].'">'.$prow[0].' - '.$prow[1].', '.$prow[2].', '.$prow[3].'</option>';
                   }
                }
@@ -104,7 +85,7 @@ $row = dbFetchRow($query);
             <?php
                $clientQuery = dbExec($db, 'select id, lastName, firstName from client');
                while( ($crow = dbFetchRow($clientQuery)) ){
-                  if($crow[0] == $row[5]){
+                  if($row[1] == $crow[0]){
                      echo '<option value="'.$crow[0].'" selected>'.$crow[0].' - '.$crow[1].', '.$crow[2].'</option>';
                   } else {
                      echo '<option value="'.$crow[0].'">'.$crow[0].' - '.$crow[1].', '.$crow[2].'</option>';
@@ -121,7 +102,7 @@ $row = dbFetchRow($query);
             <?php
                $associateQuery = dbExec($db, 'select id, lastName, firstName from employee where id in (select id from associate)');
                while( ($arow = dbFetchRow($associateQuery)) ){
-                  if($arow[0] == $row[6]){
+                  if($row[2] == $arow[0]){
                      echo '<option value="'.$arow[0].'" selected>'.$arow[0].' - '.$arow[1].', '.$arow[2].'</option>';
                   } else {
                      echo '<option value="'.$arow[0].'">'.$arow[0].' - '.$arow[1].', '.$arow[2].'</option>';
@@ -132,12 +113,23 @@ $row = dbFetchRow($query);
          </td>
       </tr>
       <tr>
+         <td>Viewing Date (Ex 12.10.1990):</td>
+         <td><input type="text" size="30" id="date" name="date" value="<?php echo $row[3] ?>" /></td>
+      </tr>
+      <tr>
+         <td>Client Comments (4000 Character limit):</td>
+         <td>
+            <textarea rows="4" cols="45" name="comments" id="comments"><?php echo $row[4] ?></textarea>
+         </td>
+      </tr>
+      <tr>
          <td colspan="2" align="center">
             <input type="submit" value="Submit" />
          </td>
       </tr>
    </table>
 </form>
+
 
 <?php
 
